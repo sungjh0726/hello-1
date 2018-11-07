d1b795ba9e9e206ddaf3cf58c73f94b6bdf86d62
indiflex <indiflex1@gmail.com>
2018-11-07 14:04:27 +0900

    test

--- t2.py
+++ t2.py
@@ -1,15 +1,49 @@
-i, sum = 0, 0
-while(i < 100) :
-    i = i +1
-    if (i % 2 == 1):
-        sum += i
+lst = ['오렌지', '사과', '배']
+dic = {'오렌지': 400, '사과': 200, '바나나': 300}
 
-print("sum=", sum)
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
-sum2 = 0
-for i in range(101) :
-    if (i % 2 == 1) :
-        sum2 += i
+print(factorial(4))
+exit()
+def default_param(a, vp = 100):
+    print(a, vp)
 
+default_param(1)
 
-print("sum2=", sum2)
+print(dic.keys())
+
+en = enumerate(lst)
+print("list=", list(en))
+
+for key in dic:
+    print("key=", key, dic[key])
+
+for i, value in enumerate(lst):
+    print("tt=",i, value)
+
+print(dic.items())
+for key, element in dic.items():
+    print("items.key=", key, ", element=", element)
+
+for i in range(0, 20, 2):
+    print(i)
+
+arr = [i ** 2 for i in range(1, 20, 2)]
+print(arr)
+
+print("최소값:", min(arr))
+print("최대값:", max(arr))
+
+outs = [f for f in lst if f != '사과']
+print(outs)
+
+outs2 = []
+for f in lst:
+    if f != '사과':
+        outs2.append(f)
+
+print(outs2)
