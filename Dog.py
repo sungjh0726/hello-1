f60a36c283f1041f54b0ab46689eeff78fe393d6
indiflex <indiflex1@gmail.com>
2018-11-08 13:24:07 +0900

    lesson source

---
+++ Dog.py
@@ -0,0 +1,53 @@
+class Dog:
+	def __init__(self, name):
+		self.name = name
+		self.color = "Blue"
+		print(self.name, "was Born")
+
+	def speak(self):
+		print("YELP!", self.name)
+
+	def __del__(self):
+			print("destroy!!")
+
+class Puppy(Dog):
+	name = 'aa'
+
+	def __init__(self):
+		# self.name = "Puppy"
+		self.color = "Red"
+		print("QQQQ> Puppy was Born", self.name)
+	
+	def __read_diary(self):
+		print("Diary content!!!!!")
+
+	def speak(self):
+		self.age = 234
+		print("Bow wow!", self.name)
+
+	def wag(self):
+		self.__read_diary()
+		print("Puppy's wag tail")
+
+	def tto():
+		print("Ttooooooooooo0000000000")
+
+class Calc:
+	def plus(a, b):
+		return a + b
+
+d = Dog('puddle')
+p = Puppy()
+d.speak()
+p.speak()
+# d.wag()
+p.wag()
+
+Puppy.tto()
+# p.tto()
+
+print("cal=", Calc.plus(1, 2))
+
+# p.__read_diary()
+
+print("aaaaa", d.name, p.name, Puppy.name)
