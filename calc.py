27bcbefe7b3bfd50189c4212d06ea6785912b00d
indiflex <indiflex1@gmail.com>
2018-11-08 11:57:28 +0900

    lesson source

--- calc.py
+++ calc.py
@@ -13,34 +13,41 @@ def div(a, b):
 
     return a / b
 
-cmd = input("수식을 입력하세요(usage: 2 + 3)> ")
-cmds = cmd.split(' ')
-
-# a = cmds[0]
-# op = cmds[1]
-# b = cmds[2]
-
-a, op, b = cmds
-# print("a=",a, ", op=", op, ", b=", b)
-
-a, b = int(a), int(b)
-
-# outType = "{:d}"
-if op == '+':
-    r = plus(a, b)
-
-elif op == '-':
-    r = minus(a, b)
-
-elif op == '*':
-    r = mul(a, b)
-
-else:
-    r = div(a, b)
 
 
-if op == '/':
-    print("Answer is {:.2f}".format(r))
+def input_calc():
+    cmd = input("수식을 입력하세요(usage: 2 + 3)> ")
+    cmds = cmd.split(' ')
 
-else: 
-    print("Answer is {:d}".format(r)) 
+    # a = cmds[0]
+    # op = cmds[1]
+    # b = cmds[2]
+
+    a, op, b = cmds
+    # print("a=",a, ", op=", op, ", b=", b)
+
+    a, b = int(a), int(b)
+
+    # outType = "{:d}"
+    if op == '+':
+        r = plus(a, b)
+
+    elif op == '-':
+        r = minus(a, b)
+
+    elif op == '*':
+        r = mul(a, b)
+
+    else:
+        r = div(a, b)
+
+
+    if op == '/':
+        print("Answer is {:.2f}".format(r))
+
+    else: 
+        print("Answer is {:d}".format(r)) 
+
+
+while(True):
+    input_calc()
