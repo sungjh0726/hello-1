3376c3c370611c5d4d7b27824117fd0c5eeb7fb6
indiflex <indiflex1@gmail.com>
2018-11-10 17:17:05 +0900

    Real Last

--- gs.sh
+++ gs.sh
@@ -1,15 +1,12 @@
-dsfdsakfjkals;dsfdsakfjkalssfdas
+#!/bin/sh
 
-a
-sdf
-dsfdsakfjkalssfdasdfasd
-dsfdsakfjkalsdsa
-f
-dsfdsakfjkalssfdasdfasdf
-as
-dsfdsakfjkals
-dsfdsakfjkalsdsa
-f
-ads
-d
-sasdfa
+DATE=`date +%Y-%m-%d" "%H:%M`
+MSG="$DATE lesson"
+if [ $# -gt 0 ]; then
+    MSG="$DATE - $1"
+fi
+
+git add --all
+git commit -am "${MSG}"
+
+git push
