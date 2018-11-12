9f1ba129e37d6360841cb99d71b46f51ce398d21
indiflex <indiflex1@gmail.com>
2018-11-12 14:21:26 +0900

    revert

--- rect2.py
+++ rect2.py
@@ -1,11 +1,9 @@
-# class Casting:
-#     def to_int(s):
-#         if type(s) == str:
-#             return int(s.strip())
-#         else:
-#             return s
-
-from Casting import Casting
+class Casting:
+    def to_int(s):
+        if type(s) == str:
+            return int(s.strip())
+        else:
+            return s
 
 class 사각형:
     name = "사각형"
