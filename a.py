3c9255c1e16f91d060d24fc4cca888920905cd37
indiflex <indiflex1@gmail.com>
2018-11-12 23:43:13 +0900

    Builtin Functions 1/2

--- a.py
+++ a.py
@@ -4,8 +4,17 @@ class TestClass:
     def __init__(self):
         print("TTTTTTTTTTTTTT")
 
+    @staticmethod
     def static_method():
         print("STATIC!!")
+
+    def set_name(self, new_name):
+        self.name = new_name
+        print("SET NAME>>", self.full_name)
+
+    @property
+    def full_name(self):
+        return self.name + " FFFF"
 
     def get_name(self):
         print("QQQQQQQQQQQQQQQQQQQQ")
@@ -30,8 +39,20 @@ class Child(TestClass):
 test = TestClass()
 child = Child()
 
-getattr(test, 'get_name')()
-getattr(TestClass, 'static_method')()
+test.static_method()
+TestClass.static_method()
+
+print("FFFFFFF>>", test.full_name)
+test.set_name("RRRRRRRRRRRRrdd")
+print("FFFFFFF>>", test.full_name)
+
+print(test.static_method)
+print(TestClass.static_method)
+
+# cmd = input("Input the function name>> ")
+
+# getattr(test, cmd)()
+# getattr(TestClass, 'static_method')()
 
 # print("11111>>", child.get_name())
 
