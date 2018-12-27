8902d83abde34bd5bbd4cde642efdaed72abf4d5
indiflex <indiflex1@gmail.com>
2018-12-27 16:44:46 +0900

    migration2 - 1

--- migration2.py
+++ migration2.py
@@ -7,13 +7,12 @@ with connection:
     # cursor를 만들어줍니다
     cursor = connection.cursor()
 
-    sql = '''select * from Departments where department_id >= :dept_id'''
+    sql = '''select region_id, region_name from Regions'''
 
     # cursor.execute(sql, dept_id=30)
-    cursor.execute(sql, (30,))
+    cursor.execute(sql)
 
     rows = cursor.fetchall()
 
 for row in rows:
     print(row)
-
