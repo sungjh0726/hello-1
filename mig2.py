39421aea089c24e496694d1a2f73e21238b3abe7
indiflex <indiflex1@gmail.com>
2019-01-04 13:17:42 +0900

    test.html login

--- mig2.py
+++ mig2.py
@@ -15,13 +15,18 @@ with connection:
     # cursor를 만들어줍니다
     cursor = connection.cursor()
 
-    sql = '''select * from Departments where department_id >= :dept_id'''
+    # sql = '''select * from Departments where department_id >= :dept_id'''
+    sql = "select * from Employees where employee_id=157"
 
     # cursor.execute(sql, dept_id=30)
-    cursor.execute(sql, (30,))
+    # cursor.execute(sql, (30,))
+    cursor.execute(sql)
 
-    rows = cursor.fetchall()
+    rows = cursor.fetchone()
 
 for row in rows:
     print(row)
 
+
+select COMMISSION_PCT, cast(COMMISSION_PCT as number(8, 10)) from Employees where employee_id = 157;
+select lengthb(COMMISSION_PCT) from Employees where employee_id = 157;
