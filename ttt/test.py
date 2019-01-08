bb908137ea446de812a31bc147a97ddddee85023
indiflex <indiflex1@gmail.com>
2019-01-09 03:18:59 +0900

    aaa

--- ttt/test.py
+++
@@ -1,7 +0,0 @@
-print("---------- This is test.py --------------")
-
-def set_dic_param(*argv, **kargv):
-	print("set params=", len(argv), '<--', argv)
-	print("dic params=", kargv.keys(), '<--', kargv)
-
-set_dic_param(1, 2, 3, name='hong', age=23)
