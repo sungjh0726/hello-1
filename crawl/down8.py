0cebca100924985aee5f1b0bf673ab909e3b485d
indiflex <indiflex1@gmail.com>
2019-01-09 12:11:36 +0900

    header

--- crawl/down8.py
+++ crawl/down8.py
@@ -2,7 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import urls
 
-bbsUrl = "https://blog.naver.com/korea_diary/221433346994"
+# bbsUrl = "https://blog.naver.com/korea_diary/221433346994"
+bbsUrl = "https://blog.naver.com/d_ahae/221436343740"
+bbsUrl = "https://blog.naver.com/kkomataku/221437665643"
+bbsUrl = "https://blog.naver.com/baekmg1988/221405485574"
 html = requests.get(bbsUrl).text
 soup = BeautifulSoup(html, 'html.parser')
 
@@ -15,7 +18,17 @@ orgHtml = requests.get(orgUrl).text
 orgSoup = BeautifulSoup(orgHtml, 'html.parser')
 
 titleSel = "div.se-title-text span"
-title = orgSoup.select_one(titleSel).text
+titleEle = orgSoup.select_one(titleSel)
+if not titleEle:
+    titleEle = orgSoup.select_one('div.se_title')
+
+if titleEle:
+    title = titleEle.text.strip()
+else:
+    title = 'Title 못찾음!! ' + bbsUrl
+
+# print("-->", title)
+# exit()
 
 sel = "img.se-image-resource"
 imgs = orgSoup.select(sel)
