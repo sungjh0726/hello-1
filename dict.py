362f5a72aa9a0208dfc9a4fe859afc19c3fefad5
indiflex <indiflex1@gmail.com>
2019-01-14 15:45:29 +0900

    dict key check

--- dict.py
+++ dict.py
@@ -13,6 +13,10 @@ print(dic)
 # 찾기
 bbbJson = dic['bbb']
 
+# 못찾을 경우 (키 존재여부 체크하기)
+if (dic.get('ccc')):
+    bbbJson2 = dic['ccc']
+
 # 수정 및 추가하기
 bbbJson['like'] = 1
 bbbJson.update({'others': 'pppppppp'})
