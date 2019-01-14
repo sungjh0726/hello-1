d5665cc4d098d6978c1309cac0cd207336efea79
indiflex <indiflex1@gmail.com>
2019-01-14 14:06:42 +0900

    dict

--- dict.py
+++ dict.py
@@ -2,13 +2,16 @@
 dic = {}
 
 # html 파싱 데이터
-dic0 = {"id": 1, "name": "aaa", "like": 0}
-dic1 = {"id": 2, "name": "bbb", "like": 0}
+dic0 = {"songno": 1, "name": "aaa", "like": 0}
+dic1 = {"songno": 2, "name": "bbb", "like": 0}
 
-dic[dic0['name']] = dic0
-dic[dic1['name']] = dic1
+# dic에 dic0과 dic1을 키값 songno로 추가하기
+dic[dic0['songno']] = dic0
+dic[dic1['songho']] = dic1
 
 print(dic)
+print("-------------------------------------")
+
 
 # 찾기
 bbbJson = dic['bbb']
