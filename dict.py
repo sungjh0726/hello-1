090aff9bda04098cebe35a485b59043a624a4c42
indiflex <indiflex1@gmail.com>
2019-01-14 14:08:06 +0900

    dict

--- dict.py
+++ dict.py
@@ -7,18 +7,19 @@ dic1 = {"songno": 2, "name": "bbb", "like": 0}
 
 # dic에 dic0과 dic1을 키값 songno로 추가하기
 dic[dic0['songno']] = dic0
-dic[dic1['songho']] = dic1
+dic[dic1['songno']] = dic1
 
 print(dic)
 print("-------------------------------------")
 
 
 # 찾기
-bbbJson = dic['bbb']
+songno = dic1['songno']
+bbbJson = dic[songno]
 
 # 수정 및 추가하기
 bbbJson['like'] = 1
 bbbJson.update({'others': 'pppppppp'})
 
-print(dic['bbb'])
+print(dic[songno])
 print(dic)
