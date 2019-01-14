e77fba2d460ddc665bd5e1b91e72a4d0fa6995a3
indiflex <indiflex1@gmail.com>
2019-01-14 13:23:26 +0900

    dict

--- dict.py
+++ dict.py
@@ -1,11 +1,16 @@
 
 dic = {}
+
+# html 파싱 데이터
 dic0 = {"id": 1, "name": "aaa", "like": 0}
 dic1 = {"id": 2, "name": "bbb", "like": 0}
+
 dic[dic0['name']] = dic0
 dic[dic1['name']] = dic1
+
 print(dic)
 
+# 찾기
 dic['bbb']['like'] = 1
 
 print(dic['bbb'])
